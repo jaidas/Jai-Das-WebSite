87a91842fc76c96e4e12713b9865d280d1ad06a6
jaidas <jaidas1987@yahoo.com>
2023-04-26 17:15:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnclosemodal = document.querySelector(".close-modal");
+const btnsopenmodal = document.querySelectorAll(".show-modal");
+
+const openmodal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closemodal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsopenmodal.length; i++)
+  btnsopenmodal[i].addEventListener("click", openmodal);
+btnclosemodal.addEventListener("click", closemodal);
+overlay.addEventListener("click", closemodal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closemodal();
+  }
+});
