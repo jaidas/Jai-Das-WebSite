e33d7fe75e3857f3ff69a5fb9e9727e8d6c08c32
jaidas <jaidas1987@yahoo.com>
2023-04-27 18:10:09 +0530

    8th commit

--- app.js
+++ app.js
@@ -34,28 +34,34 @@ const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
 const window = dom.window;
 const document = window.document;
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnclosemodal = document.querySelector(".close-modal");
-const btnsopenmodal = document.querySelectorAll(".show-modal");
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const btnclosemodal = document.querySelector(".close-modal");
+  const btnsopenmodal = document.querySelectorAll(".show-modal");
 
-const openmodal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+  const openmodal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
 
-const closemodal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+  const closemodal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
 
-for (let i = 0; i < btnsopenmodal.length; i++)
-  btnsopenmodal[i].addEventListener("click", openmodal);
-btnclosemodal.addEventListener("click", closemodal);
-overlay.addEventListener("click", closemodal);
+  for (let i = 0; i < btnsopenmodal.length; i++)
+    btnsopenmodal[i].addEventListener("click", openmodal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closemodal();
+  if (btnclosemodal) {
+    btnclosemodal.addEventListener("click", closemodal);
   }
+
+  overlay.addEventListener("click", closemodal);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      closemodal();
+    }
+  });
 });
