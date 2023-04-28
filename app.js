7d4ae666c8ce814b3a209261234373471374caa9
jaidas <jaidas1987@yahoo.com>
2023-04-28 12:41:32 +0530

    12th commit

--- app.js
+++ app.js
@@ -25,31 +25,29 @@
 // });
 
 if (typeof window !== "undefined") {
-  if (typeof window !== "undefined") {
-    const modal = document.querySelector(".modal");
-    const overlay = document.querySelector(".overlay");
-    const btnclosemodal = document.querySelector(".close-modal");
-    const btnsopenmodal = document.querySelectorAll(".show-modal");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const btnclosemodal = document.querySelector(".close-modal");
+  const btnsopenmodal = document.querySelectorAll(".show-modal");
 
-    const openmodal = function () {
-      modal.classList.remove("hidden");
-      overlay.classList.remove("hidden");
-    };
+  const openmodal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
 
-    const closemodal = function () {
-      modal.classList.add("hidden");
-      overlay.classList.add("hidden");
-    };
+  const closemodal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
 
-    for (let i = 0; i < btnsopenmodal.length; i++)
-      btnsopenmodal[i].addEventListener("click", openmodal);
-    btnclosemodal.addEventListener("click", closemodal);
-    overlay.addEventListener("click", closemodal);
+  for (let i = 0; i < btnsopenmodal.length; i++)
+    btnsopenmodal[i].addEventListener("click", openmodal);
+  btnclosemodal.addEventListener("click", closemodal);
+  overlay.addEventListener("click", closemodal);
 
-    document.addEventListener("keydown", function (e) {
-      if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-        closemodal();
-      }
-    });
-  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      closemodal();
+    }
+  });
 }
