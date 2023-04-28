777a203d342492b69e04feea786465378cfd2d08
jaidas <jaidas1987@yahoo.com>
2023-04-28 12:12:13 +0530

    9th commit

--- app.js
+++ app.js
@@ -1,67 +1,64 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnclosemodal = document.querySelector(".close-modal");
+const btnsopenmodal = document.querySelectorAll(".show-modal");
+
+const openmodal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closemodal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsopenmodal.length; i++)
+  btnsopenmodal[i].addEventListener("click", openmodal);
+btnclosemodal.addEventListener("click", closemodal);
+overlay.addEventListener("click", closemodal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closemodal();
+  }
+});
+
 // const jsdom = require("jsdom");
 // const { JSDOM } = jsdom;
 
-// const modal = document.querySelector(".modal");
-// const overlay = document.querySelector(".overlay");
-// const btnclosemodal = document.querySelector(".close-modal");
-// const btnsopenmodal = document.querySelectorAll(".show-modal");
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// const window = dom.window;
+// const document = window.document;
 
-// const openmodal = function () {
-//   modal.classList.remove("hidden");
-//   overlay.classList.remove("hidden");
-// };
+// document.addEventListener("DOMContentLoaded", function () {
+//   const modal = document.querySelector(".modal");
+//   const overlay = document.querySelector(".overlay");
+//   const btnclosemodal = document.querySelector(".close-modal");
+//   const btnsopenmodal = document.querySelectorAll(".show-modal");
 
-// const closemodal = function () {
-//   modal.classList.add("hidden");
-//   overlay.classList.add("hidden");
-// };
+//   const openmodal = function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//   };
 
-// for (let i = 0; i < btnsopenmodal.length; i++)
-//   btnsopenmodal[i].addEventListener("click", openmodal);
-// btnclosemodal.addEventListener("click", closemodal);
-// overlay.addEventListener("click", closemodal);
+//   const closemodal = function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+//   };
 
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-//     closemodal();
+//   for (let i = 0; i < btnsopenmodal.length; i++)
+//     btnsopenmodal[i].addEventListener("click", openmodal);
+
+//   if (btnclosemodal) {
+//     btnclosemodal.addEventListener("click", closemodal);
 //   }
+
+//   overlay.addEventListener("click", closemodal);
+
+//   document.addEventListener("keydown", function (e) {
+//     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//       closemodal();
+//     }
+//   });
 // });
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-const window = dom.window;
-const document = window.document;
-
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.querySelector(".modal");
-  const overlay = document.querySelector(".overlay");
-  const btnclosemodal = document.querySelector(".close-modal");
-  const btnsopenmodal = document.querySelectorAll(".show-modal");
-
-  const openmodal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
-
-  const closemodal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
-
-  for (let i = 0; i < btnsopenmodal.length; i++)
-    btnsopenmodal[i].addEventListener("click", openmodal);
-
-  if (btnclosemodal) {
-    btnclosemodal.addEventListener("click", closemodal);
-  }
-
-  overlay.addEventListener("click", closemodal);
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-      closemodal();
-    }
-  });
-});
